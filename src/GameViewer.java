25a6b8129c1bb5c6d389734187d3662f5e90a1c8
Mouzzia <mouzzia@rose-hulman.edu>
2025-05-10 22:47:35 -0400

    Game Viewer and Game Title

---
+++ src/GameViewer.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class GameViewer{
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Among Raptors");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
+
