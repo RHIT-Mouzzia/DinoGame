17f4c313948c9ac55096da85baea374bfdbbd6bd
Mouzzia <mouzzia@rose-hulman.edu>
2025-05-18 18:35:36 -0400

    Working on the Player and entity class. Adding sprites and animation. Adding controls and images corresponding to animation sequences.

--- src/GameObjects/Entities.java
+++ src/GameObjects/Entities.java
@@ -1,6 +1,8 @@
 package GameObjects;
 
 public class Entities {
-	public int x,y;
+	public int x, y;
 	public int speed;
+	
+	public 
 }
--- src/GameObjects/Player.java
+++ src/GameObjects/Player.java
@@ -1,36 +1,63 @@
 package GameObjects;
 
+import java.awt.image.*;
 import java.awt.*;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import GameObjects.Player;
 import main.GamePanel;
 import main.KeyHandler;
 
 public class Player extends Entities {
-	GamePanel gp;
-	KeyHandler keyH;
+	private GamePanel gp;
+	private KeyHandler keyH;
+	public int x, y, speed;
+	public String direction;
+	private BufferedImage upRight, upLeft, downRight, downLeft, left1, left2, right1, right2;
 
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
 		this.keyH = keyH;
+		setDefaultValues();
+		getPlayerImage();
 	}
 
 	public void setDefaultValues() {
-
 		x = 100;
 		y = 100;
 		speed = 4;
+		direction = "down";
+	}
+
+	public void getPlayerImage() {
+		try {
+			upRight = ImageIO.read(getClass().getResourceAsStream("/Images/up up.png"));
+			upLeft = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+			downRight = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+			downLeft = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/Images/up right.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	public void update() {
-		if (keyH.up == true) {
+		if (keyH.up) {
+			direction = "up";
 			y -= speed;
-		} else if (keyH.down == true) {
+		} else if (keyH.down) {
+			direction = "down";
 			y += speed;
-		} else if (keyH.left == true) {
+		} else if (keyH.left) {
+			direction = "left";
 			x -= speed;
-		} else if (keyH.right == true) {
+		} else if (keyH.right) {
+			direction = "right";
 			x += speed;
 		}
 	}
